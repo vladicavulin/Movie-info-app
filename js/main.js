3c00d673f66f2b290a806f796ef37a067ab7d1c7
vladica <vladicavulin@gmail.com>
2020-06-23 11:59:52 +0200

    Trying out the getMovie function. So far so good :)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(() => {
 $('#searchForm').on('submit', (event) => {
-console.log($('#searchText').val());    
+let searchText = $('#searchText').val(); 
+getMovie(searchText);   
 event.preventDefault();
 });
 });
+
+function getMovie(searchText) {
+   axios.get('http://www.omdbapi.com?s='+ searchText +'&apikey=ffecf8fa')
+.then((response) => {
+console.log(response);
+})
+.catch((error) => {
+console.log(error);
+})   
+};
