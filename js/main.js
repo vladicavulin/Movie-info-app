10f10d93fa20674fce733f4efbe509e7ea876861
vladica <vladicavulin@gmail.com>
2020-06-23 12:22:43 +0200

    Starting to work on the movie.html

--- js/main.js
+++ js/main.js
@@ -10,6 +10,24 @@ function getMovie(searchText) {
    axios.get('http://www.omdbapi.com?s='+ searchText +'&apikey=ffecf8fa')
 .then((response) => {
 console.log(response);
+let movies = response.data.Search;
+let output='';
+$.each(movies, (index, data) => {
+    output += `
+    <div class="col-md-3">
+    <div class="well text-center">
+    <img src="${data.Poster}">
+    <h5>${data.Title}</h5>
+    <a onclick="movieSelected('${data.imdbID}')"
+       class="btn btn-primary"
+       href="#"> Movie Details <a/>
+    </div>
+    </div>
+    `;
+});
+
+$('movies').html(output);
+
 })
 .catch((error) => {
 console.log(error);
