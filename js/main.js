7d24b3ee5e34bed743c89197b65d499e9eaa8a26
vladica <vladicavulin@gmail.com>
2020-06-24 13:44:56 +0200

    The getMovie function is working. Doing some finishing touches.

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ let movies = response.data.Search;
 let output='';
 $.each(movies, (index, data) => {
     output += `
-    <div class="col-md-3">
+    <div class="col-md-3 fire">
     <div class="well text-center">
     <img src="${data.Poster}">
     <h5>${data.Title}</h5>
@@ -46,6 +46,38 @@ function getMovie() {
     axios.get('http://www.omdbapi.com?i='+ movieId +'&apikey=ffecf8fa')
     .then((response) => {
     console.log(response);
+    let movie = response.data;
+
+    let output = `
+    <div class="row"> 
+    <div class="col-md-4">
+    <img src="${movie.Poster}" class="thumbnail">
+    </div>
+    <div class="col-md-8">
+    <h2>${movie.Title}</h2>
+    <ul class="list-group">
+    <li class="list-group-item"> <strong> Genre: </strong> ${movie.Genre}<li>
+    <li class="list-group-item"> <strong> Released: </strong> ${movie.Released}<li>
+    <li class="list-group-item"> <strong> Rated: </strong> ${movie.Rated}<li>
+    <li class="list-group-item"> <strong> IMDB Rating: </strong> ${movie.imdbRating}<li>
+    <li class="list-group-item"> <strong> Director: </strong> ${movie.Director}<li>
+    <li class="list-group-item"> <strong> Writer: </strong> ${movie.Writer}<li>
+    <li class="list-group-item"> <strong>Actors: </strong> ${movie.Actors}<li>
+    </ul>
+    </div>
+    </div>
+    <div class="row">
+    <div class="well">
+    <h3>Plot</h3>
+    ${movie.Plot}
+    <hr>
+    <a href="http://imdb.com/title/${movie.imdbId}" target="_blank" class="btn btn-primary">View IMDB</a>
+    <a href="index.html" class="btn btn-default"> Go back to search </a>
+    </div>
+    </div>
+
+    `
+$('#movie').html(output);
 
 })
 .catch((error) => {
